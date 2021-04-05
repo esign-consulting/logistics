18662584f0e4c3bc890fd5751d4ee78f42a036c7
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2021-04-05 20:09:51 +0100

    local test

--- test.sh
+++ test.sh
@@ -2,6 +2,11 @@
 
 # Init local environment
 docker-compose up -d
+if [ $? -ne 0 ]; then
+    echo "Failure on local environment initialisation. Aborting..."
+    docker-compose down
+    exit 1
+fi
 
 # Wait until Logistics is up and running
 until [ $(curl -w '%{http_code}' -o /dev/null -s http://localhost:8080/logistics/) -eq 200 ]; do
@@ -14,6 +19,28 @@ git submodule update --init --recursive
 
 # Run the API test
 mvn -f test-restassured test -Dserver.host=http://localhost -Dserver.port=8080
+if [ $? -ne 0 ]; then
+    echo "Failure on API test. Aborting..."
+    docker-compose down
+    exit 1
+fi
+
+# Run the UI test
+docker run --rm --network host -e LOGISTICS_URL=http://localhost:8080/logistics esignbr/logistics-test-selenium
+if [ $? -ne 0 ]; then
+    echo "Failure on UI test. Aborting..."
+    docker-compose down
+    exit 1
+fi
+
+# Run the Load test
+mvn -f test-jmeter verify -Dserver.host=localhost -Dserver.port=8080
+if [ $? -ne 0 ]; then
+    echo "Failure on Load test. Aborting..."
+    docker-compose down
+    exit 1
+fi
 
 # Shutdown local environment
 docker-compose down
+exit 0
